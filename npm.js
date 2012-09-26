16c5546b93e6dd78e17cab3cc7549e7451f1e4df
Fernando <ferlores@gmail.com>
2012-09-25 21:34:12 -0300

    Add twit timelines

--- npm.js
+++ npm.js
@@ -3,13 +3,21 @@ var connect = require('connect')
   , dirSamples = __dirname + '/samples/'
   , interval = 5 * 60 * 1000
   , samples = []
+  , keys = {
+      'consumer_key': process.env.consumer_key
+    , 'consumer_secret' : process.env.consumer_secret
+    , 'access_token_key' : process.env.access_token_key
+    , 'access_token_secret': process.env.access_token_secret
+  }
+  , tu = require('tuiter')(keys)
+
   
 /*****************************************************************************/
 
 fs.readdir(dirSamples, function (err, list) {
   if (err) throw new Error('Cannot read samples folder')
   list.forEach(function (name) {
-    samples.push(require(dirSamples + name));
+    samples.push(require(dirSamples + name))
   })
 })
 
@@ -48,14 +56,44 @@ var app = connect()
 io.set('log level', 1)
 
 io.sockets.on('connection', function (socket) {
+  // emit data
   db.collection('status', function (err, collection) {
-
     ['registry', 'website'].forEach(function (action){
       collection.find({action: action}).sort({'time': -1}).limit(1)
         .nextObject(function(err, results) {
           socket.emit('update', results)
-        });
+        })
     })
-    
   })
-});
+  socket.emit('twit', twits)  
+})
+
+
+var twits = []
+twits.push = function (twit) {
+  Array.prototype.push.call(this, twit)
+  io.sockets.emit('twit', twit)
+}
+
+var extractTweet = function (tweet) {
+  return {
+      screen_name: tweet.user.screen_name
+    , id: tweet.id 
+    , text: tweet.text 
+    , profile_image_url: tweet.user.profile_image_url
+  }
+}
+
+// Get the last 20 twits at startup
+tu.userTimeline({screen_name: 'npmjs'}, function (er, res) {
+  res.forEach(function (tweet) {
+    twits.unshift(extractTweet(tweet))
+  })
+})
+
+// 179132846 my twitterID for testing
+tu.filter({follow: ['309528017']}, function (stream) {
+  stream.on('tweet', function (tweet) {
+    twits.push(extractTweet(tweet))
+  })
+})
--- public/js/watch.js
+++ public/js/watch.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
   var socket = io.connect('')
   socket.on('update', function (data) {
-    update(data)
+    updateStatus(data)
+  })
+
+  socket.on('twit', function (data) {
+    if (!$.isArray(data)) data = [data]
+    $.each(data, function (idx, twit) {
+      $('#twits .content').prepend($(renderTwit(twit)))
+    })
   })
 })
 
-function update (data) {
+function updateStatus (data) {
   var elem = $('#' + data.action)
     , className = data.statusOK ? 'ok' : 'fail' 
     , ago = Math.round( (Date.now()/1000 - data.time) / 60)
@@ -14,3 +21,14 @@ function update (data) {
   elem.find('.status').html('<p>Last update:<br>' + ago + ' minutes ago</p><p>' + 
     'Request: ' + Math.round(data.timeElapsed*100)/100 + 's</p>')
 }
+
+function renderTwit(twit) {
+  var html = '<div class="twit twit-' + twit.id + '">';
+
+  html += '<img  " src="' + twit.profile_image_url + '" />';
+  html += '<p class="text"><span class="username"><a href="https://twitter.com/' + twit.screen_name + '"target=_blank rel="external">' + twit.screen_name + '</a>:</span> ';
+  html += twit.text;
+  html += '</p></div>';
+  return html;
+}
+
