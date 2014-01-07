aa88450a6ef5fe88cb3625229d92509dccab6c1e
ferlores <ferlores@gmail.com>
2014-01-07 17:33:54 -0300

    small fix for updating the classname in the icons

--- public/js/watch.js
+++ public/js/watch.js
@@ -27,7 +27,7 @@ function updateStatus (data) {
     , className = data.statusOK ? 'ok' : 'fail' 
     , ago = Math.round( (Date.now()/1000 - data.time) / 60)
 
-  elem.addClass(className)
+  elem.removeClass().addClass(className)
   elem.find('.status').html('<p>Last update:<br>' + ago + ' minutes ago</p><p>' +
     'Request: ' + Math.round(data.timeElapsed*100)/100 + 's</p>')
 }
